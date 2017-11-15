b710e9a9861e2ae0722c0ca67fc919293ff7a0d6
Irina Badigina <badigina.irina@gmail.com>
2017-11-15 21:01:28 +0300

    Basic reaction on friday provided

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const Telegraf = require('telegraf');
+const FRIDAY_STICKER_ID = 'CAADAgADHAADRq-QAvZvsAXDQGS1Ag';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+let spokeThatWeek = false;
+
+bot.start(ctx => {
+    return ctx.reply('Йоу! Я здесь, чтобы вы не пропустили ни одного викли ретро')
+});
+
+bot.hears(/пятниц/i, ctx => {
+    let messageDate = new Date(ctx.update.message.date * 1000);
+    if (!spokeThatWeek && messageDate.getDay() <= 5) {
+        ctx.replyWithSticker(FRIDAY_STICKER_ID);
+        ctx.reply('Давайте тусить!');
+        spokeThatWeek = true;
+    }
+});
+
 
 bot.startPolling();
